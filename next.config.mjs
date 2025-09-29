1c75a1ce06e0e908f77ee09698e2a6b5010983d4
“kbrycz” <karlrbrycz@gmail.com>
2025-09-29 18:11:18 -0500

    adding: '

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: "/contact", destination: "/", permanent: true },
+      { source: "/ad/chicken-coop", destination: "/ad/facebook", permanent: true },
+      { source: "/chicken-coops", destination: "/", permanent: true },
+      { source: "/concrete-foundations", destination: "/", permanent: true },
+      { source: "/pole-barns", destination: "/", permanent: true },
+      { source: "/sheds", destination: "/", permanent: true },
+      { source: "/flooring", destination: "/", permanent: true },
+      { source: "/general-contracting", destination: "/", permanent: true },
+      { source: "/gallery", destination: "/", permanent: true },
+    ];
+  },
+};
 
 export default nextConfig;
